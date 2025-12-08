030fae9b84cd4f37ad8574c50f575535aa1fbc77
nicechann <nicechann@gmail.com>
2025-12-08 18:52:10 +0900

    Initial Commit

---
+++ TriClean/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TriClean
+//
+//  Created by changyu Kang on 08/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TriClean/TriCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriCleanApp.swift
+//  TriClean
+//
+//  Created by changyu Kang on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TriCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
